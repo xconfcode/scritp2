9cd4902bb4c7b1754b9bca7b7c9761cdbae7243f
conflink <117584366+xconfcode@users.noreply.github.com>
2024-05-05 08:26:39 +0100

    Update n2.sh

--- n2.sh
+++ n2.sh
@@ -39,19 +39,20 @@ pacman -Syy archlinux-keyring --needed --noconfirm
 # ==========================================================================
 
 # Time
-
+clear
 echo -e "\nSetting Times...\n"
 ln -sf /usr/share/zoneinfo/America/New_York   /etc/localtime
 clear
 echo -e "\nSuccessfly configure Times....\n"
 
 time sleep 3
-
+clear
 sudo sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sudo sed -i 's/^#en_US ISO-8859-1/en_US ISO-8859-1/' /etc/locale.gen
 echo -e "\nSuccessfly Uncomment for locale-gen....\n"
 
 time sleep 3
+clear
 locale-gen
 hwclock --systohc
 echo LANG=en_US.UTF-8 > /etc/locale.conf
@@ -60,6 +61,7 @@ echo arch-pc > /etc/hostname
 echo -e "\nSuccessfly Uncomment for locale-gen....\n"
 
 time sleep 3
+clear
 # ==========================================================================
 #               [Start ::  Configure Accounts]
 # ==========================================================================
@@ -74,7 +76,7 @@ echo -e "\nSuccessfly user created...\n "
 # ==========================================================================
 #               [Start:: Hostname]
 # ==========================================================================
-
+clear
 echo -e "\nConfigure hostname...\n"
 # Configure hostname and hosts file
 
@@ -87,21 +89,34 @@ cat << EOF > /etc/hosts
 EOF
 echo -e "\nHostName successfully Configure...\n"
 
+
+time sleep 3
+clear
 # ==========================================================================
 #               [Installing GRUB && Depandency]
 # ==========================================================================
 
 #  Create boot dir and mount in it
 
+echo -e "\nInstalling grub and dependencies...\n"
 pacman -S grub efibootmgr os-prober mtools networkmanager network-manager-applet wpa_supplicant dialog base-devel linux-headers  cups openssh blueman git intel-ucode nano vim neovim   --noconfirm
+echo -e "\nSuccessfly installed grub & dependancy....\n"
+
+time sleep 3
+clear
+
 
 echo "Creating boot directory"
 mkdir /boot/efi
 mount  /dev/sda1 /boot/efi
 echo "Successfly Mounted & created /boot/efi directory !!!!"
+time sleep 3
+clear
+
 
 #  installing grub and dependency by pacman
 echo "installing grub and dependancy"
+time sleep 3
 
 # ==========================================================================
 #               [ GRUB]
@@ -110,11 +125,25 @@ echo "installing grub and dependancy"
 # Bootloader installation (UEFI systems)
 echo -e "\n install by grub...\n"
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi 
+
+echo -e "\nggrub install bootloader !!!!....\n"
+
+time sleep 3
+clear
+
 echo -e "\nget grub confinguration...\n"
 grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "\ngrub take configuration !!!!....\n"
+
+time sleep 3
+clear
+
 
 pacman -S nvidia nvidia-utils --needed --noconfirm
+echo -e "\ninstalled video driver !!!!....\n"
 
+time sleep 3
+clear
 
 # ==========================================================================
 #               [Enabling Syetems]
@@ -122,3 +151,13 @@ pacman -S nvidia nvidia-utils --needed --noconfirm
 
 # Enable essential services
 systemctl enable NetworkManager bluetooth cups sshd
+
+clear 
+echo -e "\nenabled NetworkManager !!!!....\n"
+echo -e "\nenabled bluetooth !!!!....\n"
+echo -e "\nenabled cups !!!!....\n"
+echo -e "\nenabled sshd !!!!....\n"
+
+echo -e "\nAll system enabled !!!!....\n"
+
+time sleep 3
