caa1c31cc2fb5da37ff936e64c5b8cce80ff19e7
conflink <117584366+xconfcode@users.noreply.github.com>
2024-05-09 18:11:20 -0800

    Update allinalldos.sh

--- allinalldos.sh
+++ allinalldos.sh
@@ -66,7 +66,8 @@ echo -e "\033[0m"  # Reset to default color
 # [[ get input ]] 
 # ==================================================================================
 
-
+#echo "Please enter EFI paritition: (example /dev/sda1 or /dev/nvme0n1p1)"
+#read EFI
 
 echo "Please enter SWAP paritition: (example /dev/sda1)"
 read SWAP
@@ -116,7 +117,7 @@ echo -e "\033[0m"  # Reset to default color
 # ==================================================================================
 # make filesystems
 echo -e "\nCreating Filesystems...\n"
-#echo -e "\nFormatting Desks\n"
+echo -e "\nFormatting Desks\n"
 #mkfs.fat -F32 /dev/"${EFI}"
 mkswap /dev/"${SWAP}"
 swapon /dev/"${SWAP}"
@@ -144,7 +145,7 @@ echo -e "\033[0m"  # Reset to default color
 # ==================================================================================
 # [[ Mounting ]] 
 # ==================================================================================
-echo -e "\nstarted Mounting only to root bcz dos scheme \n"
+echo -e "\nstarted Mounting\n"
 mount  /dev/"${ROOT}" /mnt
 #mkdir /boot/efi
 #mount  /dev/"${EFI}" /boot/efi
@@ -205,13 +206,10 @@ echo -e "\033[0m"  # Reset to default color
 # Generate Mount
 echo " Storring mount"
 genfstab -U /mnt >> /mnt/etc/fstab
-clear 
-echo "Successfly storred all mount" 
-echo -e "\n check Mount stored...\n"
-cat /mnt/etc/fstab
-
+echo "Successfly storred all mount"
 time sleep 3
-
+cat /mnt/etc/fstab
+time sleep 6
 clear
 echo -e "\033[32m"  # Start green text
 
@@ -268,6 +266,13 @@ echo -e "\033[0m"  # Reset to default color
 
 
 
+
+
+
+
+
+
+echo -e "\n Storred succefully\n" 
 time sleep 3
 #pacman -Syy archlinux-keyring --needed --noconfirm
 clear
@@ -393,17 +398,14 @@ echo -e "\033[0m"  # Reset to default color
 # ==========================================================================
 #               [Start ::  Configure Accounts]
 # ==========================================================================
-echo -e "\nstart creating user...\n"
-
 echo -e "\nEnter root password"  # Newline with echo -e
 passwd
-
+echo -e "\nstart creating user...\n"
 useradd -m -s /bin/bash "$USER"
 echo "$USER:$PASSWORD" | chpasswd
 usermod -aG wheel "$USER"
 echo -e "\nSuccessfly user created...\n "
 clear
-
 
 echo "uncomment for profile wheels"
 # 
@@ -416,12 +418,6 @@ time sleep 3
 EDITOR=nano visudo 
 
 
-
-
-
-
-echo -e "\n Storred succefully\n" 
-clear
 echo -e "\033[32m"  # Start green text
 
 cat <<EOF
@@ -505,7 +501,7 @@ echo -e "\033[0m"  # Reset to default color
 #  Create boot dir and mount in it
 
 echo -e "\nInstalling grub and dependencies...\n"
-pacman -S grub efibootmgr os-prober mtools dosfstools networkmanager network-manager-applet wpa_supplicant dialog base-devel linux-headers  cups openssh blueman git  nano vim neovim   --noconfirm
+pacman -S grub efibootmgr os-prober mtools networkmanager network-manager-applet wpa_supplicant dialog base-devel linux-headers  cups openssh blueman git intel-ucode nano vim neovim   --noconfirm
 echo -e "\nSuccessfly installed grub & dependancy....\n"
 
 time sleep 3
@@ -532,11 +528,11 @@ EOF
 echo -e "\033[0m"  # Reset to default color
 
 
-echo "Creating boot directory ..skiping"
+#echo "Creating boot directory"
 #mkdir /boot/efi
 #mount  /dev/sda1 /boot/efi
 #echo "Successfly Mounted & created /boot/efi directory !!!!"
-#time sleep 3
+time sleep 3
 clear
 clear
 
@@ -588,9 +584,9 @@ echo -e "\033[0m"  # Reset to default color
 
 # Bootloader installation (UEFI systems)
 echo -e "\n install by grub...\n"
-grub-install --target=i386-pc  /dev/sda  
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi 
 
-echo -e "\ngrub install bootloader !!!!....\n"
+echo -e "\nggrub install bootloader !!!!....\n"
 
 time sleep 3
 clear
@@ -682,7 +678,7 @@ echo -e "\nenabled sshd !!!!....\n"
 
 echo -e "\nAll system enabled !!!!....\n"
 echo -e "\nInstallation complete. Reboot using 'reboot' !!!!....\n"
-echo -e "\nDont forget typr umount -a /mnt.....!!!!....\n"
+echo -e "\nDont forget typr umount -R /mnt.....!!!!....\n"
 
 time sleep 3
 EOF
@@ -699,4 +695,8 @@ time sleep 3
 arch-chroot /mnt /bin/bash /n2.sh
 time sleep 3
 
-
+# Reboot message
+'"
+echo -e "\nInstallation complete. Reboot using 'reboot' !!!!....\n"
+echo -e "\nDont forget typr umount -R /mnt.....!!!!....\n"
+time sleep 3
